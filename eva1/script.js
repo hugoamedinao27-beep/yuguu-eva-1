c1ad6aef21d2be10ff0ecc943e3854a9e642140d
hugoamedinao27-beep <hugoamedinao27@gmail.com>
2026-04-06 15:00:16 -0400

    Hugo Medina
    1.0

---
+++ eva1/script.js
@@ -0,0 +1,3 @@
+document.getElementById('dynamicButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').textContent = '¡Genial tenemos nuestro primer Script y ademas presionaste en el boton!';
+});
